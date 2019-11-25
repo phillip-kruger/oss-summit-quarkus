c93b0de3485c5e45d289df951504c47aef0b1674
Phillip Kruger <phillip.kruger@gmail.com>
2019-11-25 09:50:03 +0200

    final Signed-off-by:Phillip Kruger <phillip.kruger@gmail.com>

--- src/main/java/org/oss/summit/HostInfoService.java
+++ src/main/java/org/oss/summit/HostInfoService.java
@@ -5,7 +5,7 @@ import javax.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class HostInfoService {
 
-    private String hostname = System.getenv().getOrDefault("HOSTNAME", "unknown");
+    private final String hostname = System.getenv().getOrDefault("HOSTNAME", "unknown");
     
     public String getHostName(){
         return this.hostname;
